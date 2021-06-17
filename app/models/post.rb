eda5f8f7eae90b99ff49d5fa230ac9876b937ac7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-17 11:13:43 +0100

    update has many comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  # belongs_to :user
-  # has_many :posts
+  belongs_to :user
+  has_many :comments
 end
