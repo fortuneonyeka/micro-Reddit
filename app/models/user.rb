0976c04faec6b005027e04e561c0c33e519c4131
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-17 10:19:37 +0100

    add validations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+
+  # has_many :posts
+  # has_many :comments
+end
+
