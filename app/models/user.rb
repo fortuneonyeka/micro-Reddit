fbc2d8387e173d317683003c7f72927bddc7594c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-17 20:49:27 +0100

    fix conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
   validates :password, presence: true, confirmation: true, length: { minimum: 8 }
